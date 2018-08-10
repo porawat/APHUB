19d4ea2a1e9c9b156d339d72e3597d51353af571
porawat <ico2upo@gmail.com>
2018-08-10 10:47:13 +0700

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -8,7 +9,10 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { Firebase_Config } from './app.firebase';
+import { FireserviceProvider } from '../providers/fireservice/fireservice';
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +22,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot({name: "__ion_APHUB"}), //ฐานข้อมูลในเครื่อง
+    AngularFireModule.initializeApp(Firebase_Config.firebase),
+    AngularFirestoreModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +35,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FireserviceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,80 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController,LoadingController,ToastController} from 'ionic-angular';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { Storage } from "@ionic/storage";
+import { FireserviceProvider} from '../../providers/fireservice/fireservice';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items: Observable<any[]>;
+  loginForm = {email:'',name:'',time:null};
+  constructor(public navCtrl: NavController,
+   public db: AngularFirestore,
+   private Fservice: FireserviceProvider,
+   private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private storage: Storage
+  ) {
+    this.items = db.collection('users').valueChanges();
   }
+  ngOnInit() {
+    this.storage.get("login-user").then(login => {
+      if (login && login.email !== "") {
+       // this.navCtrl.push(ChatsPage); ไปหาที่ต้องการ
+       console.log(login);
+      }
+    });
+  }
+  loginUser() {
+
+    console.log(this.loginForm);
+    if (this.loginForm.email === '') {
+      let myLoader = this.loadingCtrl.create({
+        content: "Please wait...",    
+      });
+      myLoader.present().then(()=>{
+        let toast = this.toastCtrl.create({
+          message: "Email ต้องไม่ว่าง",
+          duration: 2000,
+          position: "top"
+        });
+        toast.present();
+        myLoader.dismiss();
+      })
+          
+    }else{
+      let myLoader = this.loadingCtrl.create({
+        content: "Please wait...",    
+      });
+      myLoader.present().then(()=>{
+        this.loginForm.time = new Date().getTime();
+        this.Fservice.addUser(this.loginForm).then(res=>{
+        this.storage.set("login-user", this.loginForm);
+                  //myLoader.dismiss();
+                  let toast = this.toastCtrl.create({
+                    message: "Login In Successful",
+                    duration: 3000,
+                    position: "top"
+                  });
+                  toast.present();
+                  myLoader.dismiss();
+                  this.loginForm = {email:'',name:'',time:new Date().getTime()};
+      }).catch(err => {
+        console.log(err);
+        let toast = this.toastCtrl.create({
+          message: "Error "+err,
+          duration: 3000,
+          position: "top"
+        });
+        toast.present();
+        myLoader.dismiss();
+      });
+      })
+      
+  }
+}
 
 }
